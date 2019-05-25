c196a40352049e294e0fd3677daf8f33c64b5281
Quattro Cento <quattrocento1@hotmail.com>
2019-05-25 11:04:31 +0800

    remove direct enu - eln

--- veos.cpp
+++ veos.cpp
@@ -8,37 +8,6 @@
 using namespace enumivo;
 using namespace std;
 
-void veos::got_enu_send_eln(const currency::transfer &transfer) {
-
-  auto to = transfer.from;
-
-  double received = transfer.quantity.amount;
-  received = received/10000;
-
-  // get ENU balance
-  double enu_balance = enumivo::token(N(enu.token)).
-	  get_balance(_self, enumivo::symbol_type(ENU_SYMBOL).name()).amount;
-  
-  enu_balance = enu_balance/10000;
-
-  // get ELN balance
-  double eln_balance = enumivo::token(N(eln.coin)).
-	  get_balance(_self, enumivo::symbol_type(ELN_SYMBOL).name()).amount;
-
-  eln_balance = eln_balance/10000;
-
-  double product = eln_balance * enu_balance;
-  double amount = eln_balance - (product / (received + enu_balance));
-
-  auto quantity = asset(10000*amount, ELN_SYMBOL);
-
-  action(
-    permission_level{_self, N(active)}, 
-    N(eln.coin), 
-    N(transfer),
-    std::make_tuple(_self, to, quantity, std::string("Received ENU, sent ELN"))).send();
-}
-
 void veos::got_enu_send_veos(const currency::transfer &transfer) {
   auto to = transfer.from;
 
@@ -67,36 +36,6 @@ void veos::got_enu_send_veos(const currency::transfer &transfer) {
     N(veos.coin), 
     N(issue),
     std::make_tuple(to, quantity, std::string("Received ENU, issued VEOS"))).send();
-}
-
-void veos::got_eln_send_enu(const currency::transfer &transfer) {
-  auto to = transfer.from;
-
-  double received = transfer.quantity.amount;
-  received = received/10000;
-
-  // get ELN balance
-  double eln_balance = enumivo::token(N(eln.coin)).
-	  get_balance(_self, enumivo::symbol_type(ELN_SYMBOL).name()).amount;
-  
-  eln_balance = eln_balance/10000;
-
-  // get ENU balance
-  double enu_balance = enumivo::token(N(enu.token)).
-	  get_balance(_self, enumivo::symbol_type(ENU_SYMBOL).name()).amount;
-
-  enu_balance = enu_balance/10000;
-
-  double product = enu_balance * eln_balance;
-  double amount = enu_balance - (product / (received + eln_balance));
-
-  auto quantity = asset(10000*amount, ENU_SYMBOL);
-
-  action(
-    permission_level{_self, N(active)}, 
-    N(enu.token), 
-    N(transfer),
-    std::make_tuple(_self, to, quantity, std::string("Received ELN, sent ENU"))).send();
 }
 
 void veos::got_eln_send_veos(const currency::transfer &transfer) {
@@ -214,22 +153,11 @@ void veos::apply(account_name contract, action_name act) {
     if (transfer.to != _self) 
       return;
     
-    if (contract == N(enu.token)) {
-      if (transfer.memo == "ELN") 
-        got_enu_send_eln(transfer);
-      else if (transfer.memo == "VEOS") 
-        got_enu_send_veos(transfer);
-      else 
-        enumivo_assert(false, "Memo must be ELN or VEOS");
-    } else if (contract == N(eln.coin)) {
-      if (transfer.memo == "ENU") 
-        got_eln_send_enu(transfer);
-      else if (transfer.memo == "VEOS") 
-        got_eln_send_veos(transfer);
-      else 
-        enumivo_assert(false, "Memo must be ENU or VEOS");
-
-    } else if (contract == N(veos.coin)) {
+    if (contract == N(enu.token)) 
+      got_enu_send_veos(transfer);
+    else if (contract == N(eln.coin)) 
+      got_eln_send_veos(transfer);
+    else if (contract == N(veos.coin)) {
       if (transfer.memo == "ENU") 
         got_veos_send_enu(transfer);
       else if (transfer.memo == "ELN") 
